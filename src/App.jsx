2ecd8776e5659f823348b6952614bf95a75a05f8
Rapturee <145434232+Rapturee@users.noreply.github.com>
2024-02-19 22:42:05 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Station from "./Station";
+import ScrollToTopButton from "./ScrollToTopButton";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Station />
+      <ScrollToTopButton />
+      
+    </>
+  );
+}
+
+export default App;
